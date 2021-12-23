55963b84754a496d25441f7dc3ed1bd7916dce1d
edvillalobos-hcp <93220993+edvillalobos-hcp@users.noreply.github.com>
2021-12-23 11:03:01 -0600

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/Demo-Org-EV/s3-bucket/aws"
+  version = "2.11.1"
+  # insert required variables here
+  
+  bucket_prefix = var.prefix
+  bucket = "my-s3-bucket"
+  
+
+  versioning = {
+    enabled = true
+}
